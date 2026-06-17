fb246d9411eb596d894c61c5f8ef9b186da566e0
Megan McGee <meganpmcgee@gmail.com>
2026-06-17 14:52:48 -0700

    feat: update Google Sheet Web App URL to new deployment

--- leads.js
+++ leads.js
@@ -6,7 +6,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // 1. CONFIGURE YOUR GOOGLE APPS SCRIPT URL HERE
     // Follow sheets_connection_guide.md to get this URL.
-    const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbzAsHQgQbctMpY6m5BMuuvkdfOzgBZ97_CuA6plo2TKJBrdjRi8fweDJxKt5qhW1ct9Uw/exec"; 
+    const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbzr8ndacofagQ1Rv68XutDC1ONvLVvHg5hltKPe5weq2kDwWLE8iIAKEEO9jHMNzLRV/exec"; 
 
     const leadForm = document.getElementById('lead-form');
     const formContainer = document.getElementById('form-container');
